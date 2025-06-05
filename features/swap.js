ebd48e8bcf1214d7640d52403ac23dff6ecfdd48
robin <139689176+robin11110000@users.noreply.github.com>
2025-06-05 18:24:13 +0530

    make it more user friendly if there is time

--- features/swap.js
+++ features/swap.js
@@ -1,7 +1,66 @@
-// features/swap.js
-function handleSwap(ctx) {
-    ctx.reply("🔄 Swap feature coming soon.");
+require('dotenv').config();
+
+const { Telegraf } = require('telegraf');
+const { Markup } = require('telegraf');
+const { getUserWallet, createUserWallet } = require('./walletManager');
+
+const handleBalance = require('./features/balance');
+const handleSend = require('./features/send');
+const { sendMiddleware } = require('./features/send');
+const handleSwap = require('./features/swap');
+const { swapMiddleware } = require('./features/swap');
+const handleStake = require('./features/stake');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Add the send middleware to handle multi-step sending
+bot.use(sendMiddleware);
+bot.use(swapMiddleware);
+
+bot.command('start', async (ctx) => {
+  const userId = ctx.from.id.toString();
+  let wallet = getUserWallet(userId);
+
+  if (!wallet) {
+    wallet = createUserWallet(userId);
+    await ctx.reply(`🆕 New wallet created:\n${wallet.address}`);
+  } else {
+    await ctx.reply(`💼 Existing wallet:\n${wallet.address}`);
   }
-  
-  module.exports = handleSwap;
-  
+
+  await ctx.reply(
+    'Choose an action:',
+    Markup.inlineKeyboard([
+      [Markup.button.callback('💰 Balance', 'balance')],
+      [Markup.button.callback('📤 Send', 'send')],
+      [Markup.button.callback('🔄 Swap', 'swap')],
+      [Markup.button.callback('📥 Stake', 'stake')],
+    ])
+  );
+});
+
+bot.action('balance', async (ctx) => {
+  await ctx.answerCbQuery();
+  console.log('Balance action triggered');
+  await handleBalance(ctx);
+});
+
+bot.action('send', async (ctx) => {
+  await ctx.answerCbQuery();
+  console.log('Send action triggered');
+  await handleSend(ctx);
+});
+
+bot.action('swap', async (ctx) => {
+  await ctx.answerCbQuery();
+  console.log('Swap action triggered');
+  await handleSwap(ctx);
+});
+
+bot.action('stake', async (ctx) => {
+  await ctx.answerCbQuery();
+  console.log('Stake action triggered');
+  await handleStake(ctx);
+});
+
+bot.launch();
